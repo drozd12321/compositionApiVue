dd97150999076a7ad6b76f918d9f2fa30662d485
Drozdov Evgeny <drozdove912@gmail.com>
2025-03-18 18:13:51 +0200

    2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,65 @@
 <script setup>
+import { computed, reactive, ref, watch } from "vue";
 import { RouterView } from "vue-router";
+import AppInfo from "./components/AppInfo.vue";
+const name = ref("VueJs");
+const textInp = ref(null);
+const framwork = reactive({
+  version: 3.5,
+  text: "Composition API ",
+});
+const firstName = ref("");
+function changeInf() {
+  name.value = "vuejs";
+}
+function changeFramork() {
+  framwork.version = 4.4;
+  framwork.text = `${framwork.text} ${name.value}`;
+}
+
+function ConsRefs() {
+  console.log(textInp.value.value);
+}
+watch(firstName, (n, o) => {
+  console.log(n);
+});
+function changeVers(num) {
+  framwork.version = num;
+}
 </script>
 
 <template>
   <div class="container">
     <div class="api">
-      <h2>Composition Api</h2>
-      <div>fff</div>
+      <h2>Composition Api {{ name }}</h2>
+
+      <AppInfo :framwork="framwork" @changeVers="changeVers"></AppInfo>
+      <button @click="changeInf">Change</button>
+      <button @click="changeFramork">Framer</button>
+      <input type="text" ref="textInp" />
+      <button @click="ConsRefs">Отправить</button>
+
+      <input type="text" placeholder="name" v-model="firstName" />
     </div>
   </div>
 </template>
 
 <style scoped>
+input {
+  margin-top: 10px;
+  border-radius: 10px;
+  padding: 7px;
+  outline: none;
+}
+button {
+  background-color: gray;
+  border: none;
+  padding: 10px;
+  color: white;
+  font-size: 15px;
+  margin-top: 10px;
+  border-radius: 10px;
+}
 .container {
   display: flex;
   flex-direction: column;
---
+++ src/components/AppInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    {{ framwork.text }}
+    {{ framwork.version }}
+    {{ comp }}
+  </div>
+  <button @click="changeVersion">ChangeVersion</button>
+</template>
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  framwork: {
+    type: Object,
+    required: true,
+  },
+});
+const emit = defineEmits(["changeVers"]);
+
+const comp = computed(() => {
+  return `${props.framwork.text} ${props.framwork.version * 2}`;
+});
+function changeVersion() {
+  emit("changeVers", 15);
+}
+</script>
+<style>
+button {
+  background-color: rgb(170, 11, 11);
+  border: none;
+  padding: 10px;
+  color: white;
+  font-size: 15px;
+  margin-top: 10px;
+  border-radius: 10px;
+}
+</style>
